ad8cebab9f96239f4573b1cd5a9383f2e391f6f7
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-06-08 23:07:43 -0400

    Updated Contact page with output in the console after submitted

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,7 +5,7 @@
  * 
  * @author Joanne Jung
  * @date June 8, 2016
- * @version 1.0.0 : Initial commit
+ * @version 1.0.1 : Updated Contact page with output in the console after submitted
  * 
  * Student ID : 300432364
  * website : http://joannejung-assign2-updated-miniportfolio.azurewebsites.net
@@ -13,37 +13,77 @@
  */
 
 //IIFE - Immediately Invoked Functiona Expression
-(function(){
+(function () {
     "use strict";
+
+    //define an array of HTML elements
     var paragraphElements = [];
-    
+
     paragraphElements[0] = document.getElementById("paragraphOne");
     paragraphElements[1] = document.getElementById("paragraphTwo");
     paragraphElements[2] = document.getElementById("paragraphThree");
-    paragraphElements[3]=document.getElementById("paragraphFour");
-    paragraphElements[4]=document.getElementById("paragraphFive");
-    
+    paragraphElements[3] = document.getElementById("paragraphFour");
+    paragraphElements[4] = document.getElementById("paragraphFive");
+
     // define your paragraphs array(most common way)
     var paragraphs = [];
-    
+
+    // create a reference to the sendButton
+    var sendButton = document.getElementById("sendButton");
+
+    // event handler function
+    function sendButtonClick(event) {
+        console.log("clicked!");
+    }
+    // create a reference to the firstName field
+    var firstName = document.getElementById("firstName");
+    var lastName = document.getElementById("lastName");
+    var email = document.getElementById("email");
+    var contactNumber = document.getElementById("contactNumber");
+    var message = document.getElementById("message");
+
+    // createa a reference to the form
+    var contactForm = document.getElementById("contactForm");
+
+    contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log("Form is submitted");
+        showSubmittedData();
+        contactForm.reset();
+
+    });
+
+    function showSubmittedData() {
+        console.log("=======================================================");
+        console.log("First Name : " + firstName.value);
+        console.log("Last Name : " + lastName.value);
+        console.log("Email address : " + email.value);
+        console.log("Contact Number : " + contactNumber.value);
+        console.log("Message : " + message.value);
+        console.log("=======================================================");
+
+    }
+
+
+
     // data for my pages   
     //index
     paragraphs[0] = "<div class='indexFirstSentence'> Loves to meet new people, but kind of shy.</div> <br><div class='picture'> <img src=Assets/shyicon.png></div><br><br><br>";
     // proejct
     paragraphs[1] = "<br><div class='indexSecondSentence'>The first project is from last semester, COMP213 - Web Interface Design, <br> and it is the main homepage of my portfolio. <br>It contains all assignments and projects I did for last year, and also little about myself.<br><a href='http://studentweb.cencol.ca/hjung3/'>Portfolio - COMP213</a> </div>";
-    paragraphs[3]="<div class='indexSecondSentence'>The second project is a web registration form I developed for a Tennis club, Thornhill Park Tennis Club.<br> <a href='http://studentweb.cencol.ca/hjung3/assignment4/assignment_4.html'>Web Form - Thornhill Park Tennis Club.</a></div>"
-    paragraphs[4]="<div class='indexSecondSentence'>The third project of mine is a website for a realtor, Maria Guadagnolo, with some of her current listings.<br> <a href='http://studentweb.cencol.ca/hjung3/Project/homepage.html'>Homepage - Maria Guadagnolo</a></div>"
+    paragraphs[3] = "<div class='indexSecondSentence'>The second project is a web registration form I developed for a Tennis club, Thornhill Park Tennis Club.<br> <a href='http://studentweb.cencol.ca/hjung3/assignment4/assignment_4.html'>Web Form - Thornhill Park Tennis Club.</a></div>"
+    paragraphs[4] = "<div class='indexSecondSentence'>The third project of mine is a website for a realtor, Maria Guadagnolo, with some of her current listings.<br> <a href='http://studentweb.cencol.ca/hjung3/Project/homepage.html'>Homepage - Maria Guadagnolo</a></div>"
     //about me
     paragraphs[2] = "<div class='indexSecondSentence'> If you can't avoid it, Enjoy it! <br>Then you will be always happy </div>";
-    
-    
-    
+
+
+
     // check to see if paragraph one exists
-   var paragraphElementsLength=paragraphElements.length;
+    var paragraphElementsLength = paragraphElements.length;
     for (var index = paragraphElementsLength; index >= 0; index--) {
-        if(paragraphElements[index]) {
-         paragraphElements[index].innerHTML = paragraphs[index];
+        if (paragraphElements[index]) {
+            paragraphElements[index].innerHTML = paragraphs[index];
         }
     }
-    
+
 })();
